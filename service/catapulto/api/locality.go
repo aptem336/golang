cc83a0ff65e32606719c1833624fb662ac9612fc
popov <aptem336@gmail.com>
2022-08-07 19:57:12 +0800

    impl locality endpoint dtos

--- service/catapulto/api/locality.go
+++ service/catapulto/api/locality.go
@@ -1,1 +1,26 @@
 package api
+
+type LocalityRequest struct {
+	Term  string `json:"term"`
+	ISO   string `json:"iso"`
+	Limit int    `json:"limit,omitempty"`
+}
+
+type LocalityResponse struct {
+	LocalityList []Locality
+}
+
+type Locality struct {
+	Id          string `json:"id"`
+	Region1     string `json:"region_1"`
+	Region1Type string `json:"region1_type"`
+	Region2     string `json:"region_2,omitempty"`
+	Region2Type string `json:"region_2_type,omitempty"`
+	Region3     string `json:"region_3,omitempty"`
+	Locality    string `json:"locality"`
+	Zip         string `json:"zip"`
+	AOID        string `json:"aoid"`
+	ShortName   string `json:"shortname"`
+	Slug        string `json:"slug"`
+	ISO         string `json:"iso,omitempty"`
+}
